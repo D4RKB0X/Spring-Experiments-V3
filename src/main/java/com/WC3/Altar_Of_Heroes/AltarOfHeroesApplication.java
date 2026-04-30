9e11e70838fc21029f77a8f35e676e2956354c80
Lain Iwakura <kobeangelovillegas12@gmail.com>
2026-04-30 16:27:46 +0800

    First Commit

---
+++ src/main/java/com/WC3/Altar_Of_Heroes/AltarOfHeroesApplication.java
@@ -0,0 +1,13 @@
+package com.WC3.Altar_Of_Heroes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AltarOfHeroesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AltarOfHeroesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/WC3/Altar_Of_Heroes/AltarOfHeroesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.WC3.Altar_Of_Heroes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AltarOfHeroesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
