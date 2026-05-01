1ea38c1956e6237b78858a6a289e6029bd6f88f2
Lain Iwakura <kobeangelovillegas12@gmail.com>
2026-05-01 09:08:48 +0800

    Added JPA Interfaces

---
+++ src/main/java/com/WC3/Altar_Of_Heroes/Repositories/BuildingRepository.java
@@ -0,0 +1,6 @@
+package com.WC3.Altar_Of_Heroes.Repositories;
+
+import com.WC3.Altar_Of_Heroes.Entities.Building;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BuildingRepository extends JpaRepository<Building, Long> {}
---
+++ src/main/java/com/WC3/Altar_Of_Heroes/Repositories/UnitRepository.java
@@ -0,0 +1,6 @@
+package com.WC3.Altar_Of_Heroes.Repositories;
+
+import com.WC3.Altar_Of_Heroes.Entities.Unit;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UnitRepository extends JpaRepository<Unit, Long> {}
